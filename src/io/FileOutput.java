36dd6fdd11ba6eae9af3c6a16368032ae2d654ad
yangfang <506528950@qq.com>
2018-10-09 19:37:19 +0800

    add file output model

---
+++ src/io/FileOutput.java
@@ -0,0 +1,39 @@
+package io;
+
+import utils.Score;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class FileOutput {
+    private String fileOutputPath;
+
+    public FileOutput(String fileOutputPath) {
+        this.fileOutputPath = fileOutputPath;
+    }
+
+    public void writeScore(List<Score> listScore) throws IOException {
+        File file = new File(this.fileOutputPath);
+        if (!file.isFile()) {
+            file.createNewFile();
+
+        }
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(this.fileOutputPath));
+        String header = "name\tscore\n";
+        writer.write(header);
+
+
+        for(Score line:listScore){
+            writer.write(line.getName() + "\t" + line.getValue() + "\n");
+
+        }
+        writer.close();
+
+    }
+
+
+}
